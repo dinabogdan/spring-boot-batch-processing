b9b9f21aea4f7af6807ced388d0474b31d2338db
unknown <bogdan.dina03@gmail.com>
2017-11-04 20:05:00 +0200

    Add the business object, processor and batch configuration classes

---
+++ src/main/java/com/freesoft/batch/processors/PersonItemProcessor.java
@@ -0,0 +1,26 @@
+package com.freesoft.batch.processors;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.batch.item.ItemProcessor;
+
+import com.freesoft.business.objects.Person;
+
+public class PersonItemProcessor implements ItemProcessor<Person, Person> {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(PersonItemProcessor.class);
+
+	@Override
+	public Person process(Person item) throws Exception {
+
+		String firstName = item.getFirstName().toUpperCase();
+		String lastName = item.getLastName().toUpperCase();
+
+		Person processedPerson = new Person(firstName, lastName);
+
+		LOGGER.info("Converting (" + item + ") into (" + processedPerson + ")");
+
+		return processedPerson;
+	}
+
+}
